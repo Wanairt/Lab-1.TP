e7dedd39a0dbfbe11a268336ef5217ff99d275ef
Wana <Wana@DESKTOP-N0VR954>
2026-03-27 15:15:34 +0400

    Сделал 3 часть

--- Lab-1.TP/Models/Calculator.cs
+++ Lab-1.TP/Models/Calculator.cs
@@ -1,16 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Lab_1.TP.Models
 {
     public class Calculator
     {
+        [Required(ErrorMessage = "Поле 'Операнд 1' обязательно для заполнения!")]
         public string? Operand1Input { get; set; }
+
+        [Compare("Operand1Input", ErrorMessage = "Значение операнда 2 должно совпадать со значением операнда 1!")]
         public string? Operand2Input { get; set; }
 
         public string? Operation { get; set; }
-
         public double Result { get; set; }
-
         public byte Operand1 { get; set; }
         public double Operand2 { get; set; }
     }
--- Lab-1.TP/Models/CalculatorController.cs
+++ Lab-1.TP/Models/CalculatorController.cs
@@ -22,51 +22,53 @@ namespace Lab_1.TP.Models
                 model.Operation = null;
                 model.Result = 0;
                 ViewBag.ComparisonValue = 10.5;
-
                 ModelState.Clear();
-
                 return View(model);
             }
 
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                byte operand1;
-                double operand2;
-                bool isValid1 = byte.TryParse(model.Operand1Input, out operand1);
-                bool isValid2 = double.TryParse(model.Operand2Input, out operand2);
+                ViewBag.ComparisonValue = 10.5;
+                return View(model);
+            }
 
-                if (isValid1 && isValid2)
-                {
-                    model.Operand1 = operand1;
-                    model.Operand2 = operand2;
+            byte operand1;
+            double operand2;
+            bool isValid1 = byte.TryParse(model.Operand1Input, out operand1);
+            bool isValid2 = double.TryParse(model.Operand2Input, out operand2);
 
-                    switch (model.Operation)
-                    {
-                        case "+":
-                            model.Result = operand1 + operand2;
-                            break;
-                        case "-":
-                            model.Result = operand1 - operand2;
-                            break;
-                        case "*":
-                            model.Result = operand1 * operand2;
-                            break;
-                        case "/":
-                            if (operand2 != 0)
-                                model.Result = operand1 / operand2;
-                            else
-                                model.Result = double.NaN;
-                            break;
-                        default:
-                            model.Result = 0;
-                            break;
-                    }
-                }
-                else
+            if (isValid1 && isValid2)
+            {
+                model.Operand1 = operand1;
+                model.Operand2 = operand2;
+
+                switch (model.Operation)
                 {
-                    model.Result = double.NaN;
+                    case "+":
+                        model.Result = operand1 + operand2;
+                        break;
+                    case "-":
+                        model.Result = operand1 - operand2;
+                        break;
+                    case "*":
+                        model.Result = operand1 * operand2;
+                        break;
+                    case "/":
+                        if (operand2 != 0)
+                            model.Result = operand1 / operand2;
+                        else
+                            model.Result = double.NaN;
+                        break;
+                    default:
+                        model.Result = 0;
+                        break;
                 }
             }
+            else
+            {
+                model.Result = double.NaN;
+            }
+
             ViewBag.ComparisonValue = 10.5;
             return View(model);
         }
